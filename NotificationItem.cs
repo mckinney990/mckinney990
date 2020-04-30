0f060414d3e1a5d5c4058c8e2d8d89b711d8c9c9
mckinney990 <61093254+mckinney990@users.noreply.github.com>
2020-04-30 16:38:19 -0400

    Add files via upload

---
+++ NotificationItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MulliganWallet
+{
+    class NotificationItem
+    {
+
+        private long _id;
+        private String _description = String.Empty;
+        public NotificationItem(long id, String description)
+        {
+            _id = id;
+            _description = description;
+        }
+
+        public long ID
+        {
+            get { return _id; }
+        }
+
+        public String Description
+        {
+            get { return _description; }
+        }
+
+    }
+}
